e31b8b66ccc78b460188911211da53a8f53e1391
vivekkushal <vivekjss1@gmail.com>
2023-05-02 00:21:00 +0530

    feat: Add Bubble Sort

--- specs/bubble-sort/bubble-sort.test.js
+++ specs/bubble-sort/bubble-sort.test.js
@@ -12,11 +12,26 @@
 
 function bubbleSort(nums) {
   // code goes here
+  let swap = false;
+  let iterations = 1;
+  do {
+    swap = false;
+    for (let i = 0; i < nums.length - iterations; i++) {
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swap = true;
+      }
+    }
+    iterations++;
+  } while (swap);
+  return nums;
 }
 
 // unit tests
 // do not modify the below code
-test.skip("bubble sort", function () {
+test('bubble sort', function () {
   const nums = [10, 5, 3, 8, 2, 6, 4, 7, 9, 1];
   const sortedNums = bubbleSort(nums);
   expect(sortedNums).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter as Router, Link, Route, Routes } from 'react-router-dom';
+import {createRoot} from 'react-dom/client';
+import {BrowserRouter as Router, Link, Route, Routes} from 'react-router-dom';
 
 import Sort from './Sort';
 import Tree from './Tree';
 
 const App = () => {
-    return (
-        <Router>
-            <div>
-                <nav>
-                    <ul>
-                        <li>
-                            <Link to="/sort">Sort</Link>
-                        </li>
-                        <li>
-                            <Link to="/">Tree</Link>
-                        </li>
-                    </ul>
-                </nav>
-                <Routes>
-                    <Route path="/sort" element={<Sort />} />
-                    <Route path="/" element={<Tree />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/sort">Sort</Link>
+            </li>
+            <li>
+              <Link to="/">Tree</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/sort" element={<Sort />} />
+          <Route path="/" element={<Tree />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 };
 
 const container = document.getElementById('app');
